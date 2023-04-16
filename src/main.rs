12cf2b82a4159d5798b3b624b5cef3afa73f13d8
HSINPA\hsinp <hsinpa@gmail.com>
2023-04-16 23:21:04 +0800

    Learning on how to create bevy ui (pre)

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,20 @@ mod utility;
 mod game;
 use bevy::prelude::*;
 use crate::CursorIcon::Default;
+use crate::utility::types;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(utility::types::InputStruct::default())
-        .add_startup_system(game::scene_setup::setup)
-        .add_system(utility::input::input_system::keyboard_input_system)
-        .add_system(game::game_system::process_car_physics_system)
-        .add_system(game::game_system::process_car_movement_system)
+
+        .add_state::<types::AppState::Menu>()
+        .add_plugin(game::game_setup::GamePlugin)
+
+        // .insert_resource(utility::types::InputStruct::default())
+
+        // .add_system(utility::input::input_system::keyboard_input_system)
+        // .add_system(game::game_system::process_car_physics_system)
+        // .add_system(game::game_system::process_car_movement_system)
         //.add_system(game::game_system::process_camera_movement_system)
         .run();
 }
--- src/utility/types.rs
+++ src/utility/types.rs
@@ -1,48 +1,17 @@
-use bevy::asset::Handle;
-use bevy::ecs::component::{Component};
-use bevy::math::Vec2;
-use bevy::prelude::Image;
-use bevy::transform::components::Transform;
+use bevy::prelude::{Component, States};
 
 #[derive(Component)]
-pub struct NPC;
-
-#[derive(Component)]
-pub struct Player;
-
-#[derive(Component)]
-pub struct Car {
-    pub velocity : Vec2,
-    pub acceleration: Vec2,
-    pub angle : f32,
+pub struct FileStruct {
+    pub name: String,
+    pub path: String,
+    pub file_type: FileType,
 }
 
-impl Default for Car {
-    fn default() -> Self {
-        Self {
-            velocity: Default::default(),
-            acceleration: Default::default(),
-            angle : 0.0,
-        }
-    }
-}
+pub enum FileType {Image, Directory}
 
-pub struct InputStruct {
-    pub mouse_position : Vec2,
-    pub move_forward: f32,
-    pub move_right: f32,
-    pub move_left: f32,
-    pub move_backward: f32,
-}
-
-impl Default for InputStruct {
-    fn default() -> Self {
-        Self {
-            mouse_position: Default::default(),
-            move_forward: Default::default(),
-            move_right: Default::default(),
-            move_left: Default::default(),
-            move_backward: Default::default(),
-        }
-    }
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum AppState {
+    #[default]
+    Menu,
+    Game,
 }
